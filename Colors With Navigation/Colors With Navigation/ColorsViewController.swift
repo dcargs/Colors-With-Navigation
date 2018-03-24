c24df399b52fa901bd6a47dcd143690d3d44b248
Devin Cargill <dcargs@icloud.com>
2018-03-23 19:51:20 -0500

    Built functionality for challenge

---
+++ Colors With Navigation/Colors With Navigation/ColorsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ColorsViewController.swift
+//  Colors With Navigation
+//
+//  Created by Devin Cargill on 3/23/18.
+//  Copyright © 2018 Devin Cargill. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        cell.textLabel?.text = colors[indexPath.row]
+        return cell
+    }
+
+}
