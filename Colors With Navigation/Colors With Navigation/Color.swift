1916b624f040d57fdb7ec03e657e7641bbe9b79c
Devin Cargill <dcargs@icloud.com>
2018-03-23 20:00:46 -0500

    Added color struct and set background color of each cell

---
+++ Colors With Navigation/Colors With Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors With Navigation
+//
+//  Created by Devin Cargill on 3/23/18.
+//  Copyright © 2018 Devin Cargill. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
